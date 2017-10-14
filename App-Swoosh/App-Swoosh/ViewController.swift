f37f06a0fc01d1b70ade7fc47b7d3e7d9b26e94f
vince zipparro <vincezipparro@vinces-mbp.lan>
2017-10-13 22:27:10 -0500

    added constraints to first view controller

--- App-Swoosh/App-Swoosh/ViewController.swift
+++ App-Swoosh/App-Swoosh/ViewController.swift
@@ -17,12 +17,7 @@ class ViewController: UIViewController {
     {
         super.viewDidLoad()
         
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 -
-            swoosh.frame.size.width / 2, y: 50, width:
-            swoosh.frame.size.width, height:
-            swoosh.frame.size.height)
         
-        bgImg.frame = view.frame
         
     }
 
